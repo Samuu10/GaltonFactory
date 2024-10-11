c2289d6a0f971e5aea5770e20fc11fe86ec31a99
Samuu10 <svallmun@myuax.com>
2024-10-11 16:44:57 +0200

    Initial commit

---
+++ src/test/java/com/galton/factory/galtonfactory/GaltonFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galton.factory.galtonfactory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaltonFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
